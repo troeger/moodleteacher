95b4bf0a4136b2895826cf6f707efde08dcc9e7e
Peter Tröger <peter@troeger.eu>
2019-03-14 15:57:11 +0100

    Reduce install dependencies

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name = 'moodleteacher',
-    version = '0.0.7',
-    url = 'https://github.com/troeger/moodleteacher',
+    name='moodleteacher',
+    version='0.0.7',
+    url='https://github.com/troeger/moodleteacher',
     license='BSD',
-    author = 'Peter Tröger',
-    description = 'A Moodle client library for teachers.',
-    author_email = 'peter@troeger.eu',
+    author='Peter Tröger',
+    description='A Moodle client library for teachers.',
+    author_email='peter@troeger.eu',
     classifiers=[
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3.6'
     ],
-    install_requires=['wxPython', 'requests', 'PyMuPDF'],
-    packages = ['moodleteacher'],
+    install_requires=['requests'],
+    extras_require={
+        'ui': ['wxPython', 'PyMuPDF']
+    },
+    packages=['moodleteacher']
 )
-
-
