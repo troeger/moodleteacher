ee8d511bf81c4efe548c36314d88e2554f1bf45b
Peter Tröger <peter@troeger.eu>
2019-03-15 13:21:08 +0100

    Generalize file handling

--- moodleteacher/files.py
+++ moodleteacher/files.py
@@ -6,14 +6,16 @@ import os
 import os.path
 import re
 import tempfile
-
+import requests
 
 from .requests import MoodleRequest
 
 
 class MoodleFolder():
     '''
-        A single folder in Moodle.
+        A single folder in Moodle. On construction,
+        all file information in the folder is also determined,
+        but the files themselves are not downloaded.
     '''
 
     def __init__(self, conn, course, raw_json):
@@ -24,71 +26,24 @@ class MoodleFolder():
         self.visible = bool(raw_json['visible'])
         self.files = []
         for file_detail in raw_json['contents']:
-            f = MoodleFile(self.conn, raw_json['filename'])
+            f = MoodleFile()
+            f.conn = self.conn
+            f.name = file_detail['filename']
             f.folder = self
-            f.size = raw_json['filesize']
-            f.url = raw_json['fileurl']
-            f.mimetype = raw_json['mimetype']
-            f.relative_path = raw_json['filepath']
-            f.owner = self.course.get_user(raw_json['userid'])
+            f.size = file_detail['filesize']
+            f.url = file_detail['fileurl']
+            f.mimetype = file_detail['mimetype']
+            f.relative_path = file_detail['filepath']
+            f.owner = self.course.get_user(file_detail['userid'])
             self.files.append(f)
 
     def __str__(self):
         return "{0.name} ({1} files)".format(self, len(self.files))
 
 
-class MoodleFile(list):
+class MoodleFile():
     '''
-        A generic Moodle file.
-    '''
-    conn = None
-    name = None
-    folder = None
-    size = None
-    url = None
-    mimetype = None
-    encoding = None
-    content_type = None
-    relative_path = ''
-    owner = None
-
-    def __init__(self, conn, name):
-        self.conn = conn
-        self.name = name
-
-    def __str__(self):
-        return "{0.filepath}{0.filename}".format(self)
-
-    def download(self):
-        '''
-        Download the file content and stores in in self.content.
-        Expects self.url to be set.
-
-        The method fills self.name, self.content_type and 
-        self.encoding with the determined information.
-        If self.name is already set, it will not be changed.
-        '''
-        assert(self.url)
-        response = requests.get(self.url, params={
-                                'token': self.conn.token})
-        self.encoding = response.encoding
-        if not self.name:
-            try:
-                disp = response.headers['content-disposition']
-                self.name = re.findall('filename="(.+)"', disp)[0]
-            except KeyError:
-                self.name = self.url.split('/')[-1]
-        self.content_type = response.headers.get('content-type')
-        self.content = response.content
-
-
-
-
-class MoodleSubmissionFile():
-    '''
-        A single student submission file in Moodle.
-
-        TODO: Migrate generic functionality from here to MoodleFile.
+        A file stored in Moodle.
     '''
     # Content types we don't know how to deal with in the preview
     UNKNOWN_CONTENT = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document',
@@ -101,19 +56,71 @@ class MoodleSubmissionFile():
     TAR_CONTENT = ['application/x-gzip', 'application/gzip', 'application/tar',
                    'application/tar+gzip', 'application/x-gtar', 'application/x-tgz']
 
-    # Shell to suer for execution
-    SHELL = ['/bin/bash', ]
-
+    conn = None
+    name = None
+    folder = None
+    size = None
+    url = None
+    mimetype = None
     encoding = None
-    filename = None
     content_type = None
     content = None
+    relative_path = ''
+    owner = None
     is_binary = None
     is_pdf = False
     is_zip = False
     is_html = False
     is_image = False
     is_tar = False
+
+    def __str__(self):
+        return "{0.relative_path}{0.name}".format(self)
+
+    def download(self):
+        '''
+        Download the file content and stores in in self.content.
+        Expects self.url to be set.
+
+        The method fills self.name, self.content_type and
+        self.encoding with the determined information.
+        If self.name is already set, it will not be changed.
+        '''
+        assert(self.conn)
+        assert(self.url)
+        response = requests.get(self.url, params={
+                                'token': self.conn.token})
+        self.encoding = response.encoding
+        if not self.name:
+            try:
+                disp = response.headers['content-disposition']
+                self.name = re.findall('filename="(.+)"', disp)[0]
+            except KeyError:
+                self.name = self.url.split('/')[-1]
+        self.content_type = response.headers.get('content-type')
+        self.content = response.content
+        self.analyze_content()
+
+    def analyze_content(self):
+        '''
+        Analyzes the content of the file and sets some information bits.
+        Expects self.content to be set.
+        '''
+        assert(self.content)
+        self.is_binary = False if isinstance(self.content, str) else True
+        if self.content_type:
+            self.is_pdf = True if 'application/pdf' in self.content_type else False
+            self.is_zip = True if 'application/zip' in self.content_type else False
+            self.is_html = True if 'text/html' in self.content_type else False
+            self.is_image = True if 'image/' in self.content_type else False
+            self.is_tar = True if self.content_type in self.TAR_CONTENT else False
+
+
+class MoodleSubmissionFile(MoodleFile):
+    '''
+        A single student submission file in Moodle.
+    '''
+    SHELL = ['/bin/bash', ]
 
     def __init__(self, *args, **kwargs):
         '''
@@ -124,44 +131,32 @@ class MoodleSubmissionFile():
         '''
         if 'filename' in kwargs and 'content' in kwargs:
             # Pseudo file
-            self.filename = kwargs['filename']
+            self.name = kwargs['filename']
             self.content = kwargs['content']
             if 'content_type' in kwargs:
                 self.content_type = kwargs['content_type']
             else:
                 # Special treatement of meta-files
-                if self.filename.startswith('__MACOSX'):
+                if self.name.startswith('__MACOSX'):
                     self.content_type = 'text/plain'
                 else:
                     self.content_type = mimetypes.guess_type(self.filename)[0]
         elif 'conn' in kwargs or 'url' in kwargs:
-            response = requests.get(kwargs['url'], params={
-                                    'token': kwargs['conn'].token})
-            self.encoding = response.encoding
-            try:
-                disp = response.headers['content-disposition']
-                self.filename = re.findall('filename="(.+)"', disp)[0]
-            except KeyError:
-                self.filename = kwargs['url'].split('/')[-1]
-            self.content_type = response.headers.get('content-type')
-            self.content = response.content
+            self.conn = kwargs['conn']
+            self.url = kwargs['url']
+            self.download()
         else:
             raise ValueError
-        self.is_binary = False if isinstance(self.content, str) else True
-        if self.content_type:
-            self.is_pdf = True if 'application/pdf' in self.content_type else False
-            self.is_zip = True if 'application/zip' in self.content_type else False
-            self.is_html = True if 'text/html' in self.content_type else False
-            self.is_image = True if 'image/' in self.content_type else False
-            self.is_tar = True if self.content_type in self.TAR_CONTENT else False
 
     def run_shellscript_local(self, args=[]):
+        assert(self.content)
         with tempfile.NamedTemporaryFile(mode='w+b' if self.is_binary else 'w+') as disk_file:
             disk_file.write(self.content)
             disk_file.flush()
             return subprocess.run([*self.SHELL, disk_file.name, *args], stderr=subprocess.STDOUT)
 
     def compile_with(self, cmd):
+        assert(self.content)
         with tempfile.TemporaryDirectory() as d:
             f = open(d + os.sep + self.filename, mode="w")
             f.write(self.content)
@@ -174,7 +169,7 @@ class MoodleSubmissionFile():
         '''
         Create a list of MoodleSubmissionFile objects from a list of URLs.
 
-        URLs for ZIP or TAR.GZ archives are also downloaded and automatically uncompressed.
+        ZIP or TAR.GZ archives are also downloaded and automatically uncompressed.
         In such a case, a single file URL may still lead to an array of MoodleSubmissionFile
         objects being returned.
         '''
@@ -189,6 +184,7 @@ class MoodleSubmissionFile():
                     data = input_zip.read(fname)
                     sub_f = MoodleSubmissionFile(
                         filename=fname, content=data)
+                    sub_f.analyze_content()
                     obj_list.append(sub_f)
             elif f.is_tar:
                 input_tar = tarfile.open(BytesIO(f.content))
@@ -197,6 +193,7 @@ class MoodleSubmissionFile():
                     data = input_tar.extractfile(info)
                     sub_f = MoodleSubmissionFile(
                         filename=info.name, content=data)
+                    sub_f.analyze_content()
                     obj_list.append(sub_f)
             else:
                 obj_list.append(f)
@@ -204,8 +201,9 @@ class MoodleSubmissionFile():
 
     def run_shellscript_remote(self, user_name, host, target_path, args=[]):
         '''
-            Copy the file to a remote SCP host and run it. Ignores authentication.
+        Copy the file to a remote SCP host and run it. Ignores authentication.
         '''
+        assert(self.content)
         with tempfile.NamedTemporaryFile(mode='w+b' if self.is_binary else 'w+') as disk_file:
             disk_file.write(self.content)
             disk_file.flush()
@@ -217,6 +215,7 @@ class MoodleSubmissionFile():
                                    ], stderr=subprocess.STDOUT)
 
     def as_text(self):
+        assert(self.content)
         if self.is_binary:
             if self.encoding:
                 return self.content.decode(self.encoding)
