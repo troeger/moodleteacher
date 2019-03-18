8addd8940c3cf2fca4d8392f585b4827afc138e8
Peter Tröger <peter@troeger.eu>
2019-03-18 12:20:14 +0100

    Port from OpenSubmit to Moodle

---
+++ tests/__init__.py
@@ -0,0 +1,22 @@
+import logging
+logger = logging.getLogger('moodleteacher')
+
+
+def assert_raises(callable, *args, **kwargs):
+    try:
+        return callable(*args, **kwargs)
+    except Exception:
+        pass
+    else:
+        logger.error("Unexpected non-occurence of exception while running " + str(callable))
+        raise SystemExit()
+
+
+def assert_dont_raises(callable, *args, **kwargs):
+    try:
+        return callable(*args, **kwargs)
+    except Exception as e:
+        logger.error("Unexpected occurence of exception while running {1}: {0} ".format(e, str(callable)))
+        raise SystemExit()
+    else:
+        pass
