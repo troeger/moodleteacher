0567890efb98eacb33b14d68e74ca884ab5a774b
Peter Tröger <peter@troeger.eu>
2019-03-20 15:37:10 +0100

    Bump version: 0.0.9 → 0.0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='moodleteacher',
-    version='0.0.9',
+    version='0.0.10',
     url='https://github.com/troeger/moodleteacher',
     license='BSD',
     author='Peter Tröger',
